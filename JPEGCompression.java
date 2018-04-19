36fb3306ac6f7f51d3a34702e86fa5acf5c9b319
Nakul Narwaria <nakul.narwaria08@gmail.com>
2018-04-19 14:22:29 -0700

    Update JPEGCompression.java

--- JPEGCompression.java
+++ JPEGCompression.java
@@ -9,16 +9,16 @@ public class JPEGCompression {
 	int[][] n = new int[tmp][];
 	int index=0;
 		for(int y=0;y<data.length/8;y++) {
-		for(int x=0;x<data[0].length/8;x++) {
-			double[][] sample = new double[8][8];
-			for(int i=0;i<8;i++ ) {
-				for(int j=0;j<8;j++) {
-					sample[i][j] = data[i + y*8][j + x*8];
-				}	
+			for(int x=0;x<data[0].length/8;x++) {
+				double[][] sample = new double[8][8];
+				for(int i=0;i<8;i++ ) {
+					for(int j=0;j<8;j++) {
+						sample[i][j] = data[i + y*8][j + x*8];
+					}	
+				}
+				n[index]= compute(sample);
 			}
-		 n[index]= compute(sample);
 		}
-	}
 		
 		
 	for(int i=0;i<n.length;i++)
@@ -41,8 +41,8 @@ public class JPEGCompression {
 		for(int i=0;i<data.length;i++ ) {
 			for(int j=0;j<data[0].length;j++) {
 				dataa[i][j]= data[i][j]-128;
+			}
 		}
-	}
 		dataa = dct(dataa);
 		dataa = quantize(dataa);
 			
@@ -81,10 +81,10 @@ public class JPEGCompression {
 						
 					}
 					else {
-					x=x-1;
-					y=y+1;
-					x=x+1;
-					direction=0;
+						x=x-1;
+						y=y+1;
+						x=x+1;
+						direction=0;
 					}
 				}
 				else {
@@ -104,10 +104,10 @@ public class JPEGCompression {
 						direction=1;
 					}
 					else {
-					x=x+1;
-					y=y-1;
-					y=y+1;
-					direction=1;
+						x=x+1;
+						y=y-1;
+						y=y+1;
+						direction=1;
 				}
 				}
 				else {
@@ -136,67 +136,67 @@ public class JPEGCompression {
 		}
 		
 		double[][] data = new double[height][width];
-	for(int i=0;i<height;i++ ) {
-		for(int j=0;j<width;j++) {
-			if(i<imageData.length && j<imageData[0].length) {
-				data[i][j] = imageData[i][j];
+		for(int i=0;i<height;i++ ) {
+			for(int j=0;j<width;j++) {
+				if(i<imageData.length && j<imageData[0].length) {
+					data[i][j] = imageData[i][j];
+				}
 			}
 		}
-	}
-	return data;
+		return data;
 	}
 
 	
 	 //DCT
     public static double[][] dct(double[][] data)
     {
-        int i,j,u,v;
-        double val;
-        double temp[][] = new double[8][8]; 
-        //outer
-        for(u=0;u<8;u++)
-        {
-            for(v=0;v<8;v++)
-            {
-                val = 0;
-                //inner
-                for(i=0;i<8;i++)
-                {
-                    for(j=0;j<8;j++)
-                    {
-                       val = val + (data[i][j]*Math.cos(Math.PI*(2*i+1)*u/16)*Math.cos(Math.PI*(2*j+1)*v/16)); 
-                    }
-                }           
-                val = val / Math.sqrt(8); //for a
-                
-                if(u!=0)
-                {
-                	val = val* Math.sqrt(2);
-                }
-               
-                val = val / Math.sqrt(8); //for b
-                
-                if(v!=0)
-                {
-                    val = val * Math.sqrt(2);
-                }
-                
-                 
-                temp[u][v] = val;
-            }
-        }
-        
-       return temp;
+	int i,j,u,v;
+	double val;
+	double temp[][] = new double[8][8]; 
+	//outer
+	for(u=0;u<8;u++)
+	{
+	    for(v=0;v<8;v++)
+	    {
+		val = 0;
+		//inner
+		for(i=0;i<8;i++)
+		{
+		    for(j=0;j<8;j++)
+		    {
+		       val = val + (data[i][j]*Math.cos(Math.PI*(2*i+1)*u/16)*Math.cos(Math.PI*(2*j+1)*v/16)); 
+		    }
+		}           
+		val = val / Math.sqrt(8); //for a
+
+		if(u!=0)
+		{
+			val = val* Math.sqrt(2);
+		}
+
+		val = val / Math.sqrt(8); //for b
+
+		if(v!=0)
+		{
+		    val = val * Math.sqrt(2);
+		}
+
+
+		temp[u][v] = val;
+	    }
+	}
+
+	return temp;
     }
      
    
     public static double[][] quantize(double [][]data)
     {
-        double qt[][] = {{16,11,10,16,24,40,51,61},{12,12,14,19,26,58,60,55},{14,13,16,24,40,57,69,56},{14,17,22,29,51,87,80,62},{18,22,37,56,68,109,103,77},{24,35,55,64,81,104,113,92},{49,64,78,87,103,121,120,101},{72,92,95,98,112,110,103,99}};
+       double qt[][] = {{16,11,10,16,24,40,51,61},{12,12,14,19,26,58,60,55},{14,13,16,24,40,57,69,56},{14,17,22,29,51,87,80,62},{18,22,37,56,68,109,103,77},{24,35,55,64,81,104,113,92},{49,64,78,87,103,121,120,101},{72,92,95,98,112,110,103,99}};
        for(int i=0;i<8;i++)
-       for(int j=0;j<8;j++){
-          data[i][j]=data[i][j]/qt[i][j];
-          data[i][j] = Math.round(data[i][j]);
+      	 for(int j=0;j<8;j++){
+		  data[i][j]=data[i][j]/qt[i][j];
+	  	  data[i][j] = Math.round(data[i][j]);
        }
       return data; 
     }
